cdf3e313163a6690093e5ce209fb9193a1a460c7
Raymond LeClair <raymond.leclair@springbok.io>
2024-03-11 13:44:43 -0400

    Use a consistent conda install

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 # Run this script in base image:
 # $ docker run -itv $PWD:/root -w /root continuumio/miniconda3@sha256:166ff37fba6c25fcad8516aa5481a2a8dfde11370f81b245c1e2e8002e68bcce
 eval "$(conda shell.bash hook)"
-conda create -n fiberseq-fire -c conda-forge -c bioconda 'snakemake>=8.4' -y
+conda env create -f update.yaml
 conda activate fiberseq-fire
 conda env export | grep -v prefix > environment.yaml
 conda deactivate
